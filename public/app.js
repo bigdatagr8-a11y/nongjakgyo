fca3ddb470991b887f183f59cfe7d500b79406bd
bigdatagr8-a11y <bigdatagr8@gmail.com>
2026-06-11 08:16:05 +0900

    Refine gradeList to include only valid grades
    Filter grades based on predefined valid grades and sort them accordingly.

--- public/app.js
+++ public/app.js
@@ -2943,7 +2943,8 @@ function App() {
       )).sort()
     : [];
 
-  var gradeList = Array.from(new Set(activeData.map(function(r){return r.grade||"";}).filter(Boolean))).sort();
+  var VALID_GRADES = ["특","상","보통","특등","상등"];
+  var gradeList = Array.from(new Set(activeData.map(function(r){return r.grade||"";}).filter(function(g){ return VALID_GRADES.indexOf(g) !== -1; }))).sort(function(a,b){ return VALID_GRADES.indexOf(a) - VALID_GRADES.indexOf(b); });
 
   // 단위 구간화
   var unitList = UNIT_RANGES.map(function(r){ return r.label; });
